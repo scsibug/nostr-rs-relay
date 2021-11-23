5224b9159ee968aa48c8666009536a800ca0e926
Greg Heartsfield <scsibug@imap.cc>
2021-11-23 10:46:47 -0600

    Structure for parsing types

--- src/error.rs
+++ src/error.rs
@@ -9,4 +9,10 @@ pub type Result<T, E = Error> = result::Result<T, E>;
 pub enum Error {
     #[error("command from client not recognized")]
     CommandNotFound,
+    #[error("parsing JSON->Event failed")]
+    EventParseFailed,
+    #[error("parsing JSON->Req failed")]
+    ReqParseFailed,
+    #[error("parsing JSON->Close failed")]
+    CloseParseFailed,
 }
