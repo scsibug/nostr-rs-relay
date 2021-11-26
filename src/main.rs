010c96b05f693775d211119c07c07e27f57b16f8
Greg Heartsfield <scsibug@imap.cc>
2021-11-26 10:17:20 -0600

    Moved nostr protocol handling to proto module

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
-use std::{env, io::Error};
-
-//use futures::stream::SplitSink;
-use futures_util::{SinkExt, StreamExt};
-use log::{debug, info, warn};
+use log::info;
 use nostr_rs_relay::proto::Proto;
-//use tokio::io::{ReadHalf, WriteHalf};
+use std::{env, io::Error};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::runtime::Builder;
-use tokio_tungstenite::WebSocketStream;
-use tungstenite::error::Error::*;
-use tungstenite::protocol::frame::coding::CloseCode;
-use tungstenite::protocol::frame::CloseFrame;
-use tungstenite::protocol::{Message, WebSocketConfig};
+use tungstenite::protocol::WebSocketConfig;
 
 fn main() -> Result<(), Error> {
     let _ = env_logger::try_init();
@@ -46,7 +38,7 @@ fn main() -> Result<(), Error> {
 // Todo: Implement sending messages to all other clients; example:
 // https://github.com/snapview/tokio-tungstenite/blob/master/examples/server.rs
 
-// Handles new TCP connections
+// Wrap/Upgrade TCP connections in WebSocket Streams, and hand off to Nostr protocol handler.
 async fn nostr_server(stream: TcpStream) {
     let addr = stream
         .peer_addr()
@@ -62,101 +54,13 @@ async fn nostr_server(stream: TcpStream) {
     match conn {
         Ok(ws_stream) => {
             info!("New WebSocket connection: {}", addr);
-            process_client(ws_stream).await;
+            // create a nostr protocol handler, and give it the stream
+            let mut proto = Proto::new(ws_stream);
+            proto.process_client().await
         }
         Err(_) => {
             println!("Error");
             info!("Error during websocket handshake");
         }
     };
-}
-
-// Handles valid clients who have upgraded to WebSockets
-async fn process_client(mut stream: WebSocketStream<TcpStream>) {
-    // get a protocol helper;
-    let mut proto = Proto::new();
-    // TODO: select on a timeout to kill non-responsive clients
-
-    while let Some(mes_res) = stream.next().await {
-        // as long as connection is not closed...
-        match mes_res {
-            Ok(Message::Text(cmd)) => {
-                info!("Message received");
-                let length = cmd.len();
-                debug!("Message: {}", cmd);
-                stream
-                    .send(Message::Text(format!(
-                        "got your message of length {}",
-                        length
-                    )))
-                    .await
-                    .expect("send failed");
-                // Handle this request. Everything else below is basically websocket error handling.
-                let proto_error = proto.process_message(cmd);
-                match proto_error {
-                    Err(_) => {
-                        stream
-                            .send(Message::Text(
-                                "[\"NOTICE\", \"Failed to process message.\"]".to_owned(),
-                            ))
-                            .await
-                            .expect("send failed");
-                    }
-                    Ok(_) => {
-                        info!("Message processed successfully");
-                    }
-                }
-            }
-            Ok(Message::Binary(_)) => {
-                info!("Ignoring Binary message");
-                stream
-                    .send(Message::Text(
-                        "[\"NOTICE\", \"BINARY_INVALID: Binary messages are not supported.\"]"
-                            .to_owned(),
-                    ))
-                    .await
-                    .expect("send failed");
-            }
-            Ok(Message::Ping(_)) | Ok(Message::Pong(_)) => debug!("Ping/Pong"),
-            Ok(Message::Close(_)) => {
-                info!("Got request to close connection");
-                return;
-            }
-            Err(tungstenite::error::Error::Capacity(
-                tungstenite::error::CapacityError::MessageTooLong { size, max_size },
-            )) => {
-                info!(
-                    "Message size too large, disconnecting this client. ({} > {})",
-                    size, max_size
-                );
-                stream.send(Message::Text("[\"NOTICE\", \"MAX_EVENT_SIZE_EXCEEDED: Exceeded maximum event size for this relay.  Closing Connection.\"]".to_owned())).await.expect("send notice failed");
-                stream
-                    .close(Some(CloseFrame {
-                        code: CloseCode::Size,
-                        reason: "Exceeded max message size".into(),
-                    }))
-                    .await
-                    .expect("failed to send close frame");
-                return;
-            }
-            Err(AlreadyClosed) => {
-                warn!("this connection was already closed, and we tried to operate on it");
-                return;
-            }
-            Err(ConnectionClosed) | Err(Io(_)) => {
-                debug!("Closing this connection normally");
-                return;
-            }
-            Err(Tls(_)) | Err(Protocol(_)) | Err(Utf8) | Err(Url(_)) | Err(HttpFormat(_))
-            | Err(Http(_)) => {
-                info!("websocket/tls/enc protocol error, dropping connection");
-                return;
-            }
-            Err(e) => {
-                warn!("Some new kind of error, bailing: {:?}", e);
-                return;
-            }
-        }
-    }
-    println!("Client is exiting, no longer reading websocket messages");
 }
--- src/proto.rs
+++ src/proto.rs
@@ -2,8 +2,17 @@ use crate::close::Close;
 use crate::error::{Error, Result};
 use crate::event::Event;
 use crate::subscription::Subscription;
-use log::{debug, info};
+use futures::SinkExt;
+use tungstenite::error::Error::*;
+use tungstenite::protocol::frame::coding::CloseCode;
+use tungstenite::protocol::frame::CloseFrame;
+use tungstenite::protocol::Message;
+
+use futures::StreamExt;
+use log::{debug, info, warn};
 use std::collections::HashMap;
+use tokio::net::TcpStream;
+use tokio_tungstenite::WebSocketStream;
 use uuid::Uuid;
 
 // A protocol handler/helper.  Use one per client.
@@ -11,20 +20,112 @@ pub struct Proto {
     client_id: Uuid,
     // current set of subscriptions
     subscriptions: HashMap<String, Subscription>,
+    // websocket
+    stream: WebSocketStream<TcpStream>,
     max_subs: usize,
 }
 
 const MAX_SUBSCRIPTION_ID_LEN: usize = 256;
 
 impl Proto {
-    pub fn new() -> Self {
+    pub fn new(stream: WebSocketStream<TcpStream>) -> Self {
         let p = Proto {
             client_id: Uuid::new_v4(),
             subscriptions: HashMap::new(),
             max_subs: 128,
+            stream: stream,
         };
         debug!("New client: {:?}", p.client_id);
         p
+    }
+
+    pub async fn process_client(&mut self) {
+        while let Some(mes_res) = self.stream.next().await {
+            self.send_notice().await;
+
+            match mes_res {
+                Ok(Message::Text(cmd)) => {
+                    info!("Message received");
+                    let length = cmd.len();
+                    debug!("Message: {}", cmd);
+                    self.stream
+                        .send(Message::Text(format!(
+                            "got your message of length {}",
+                            length
+                        )))
+                        .await
+                        .expect("send failed");
+                    // Handle this request. Everything else below is basically websocket error handling.
+                    let proto_error = self.process_message(cmd);
+                    match proto_error {
+                        Err(_) => {
+                            self.stream
+                                .send(Message::Text(
+                                    "[\"NOTICE\", \"Failed to process message.\"]".to_owned(),
+                                ))
+                                .await
+                                .expect("send failed");
+                        }
+                        Ok(_) => {
+                            info!("Message processed successfully");
+                        }
+                    }
+                }
+                Ok(Message::Binary(_)) => {
+                    info!("Ignoring Binary message");
+                    self.stream
+                        .send(Message::Text(
+                            "[\"NOTICE\", \"BINARY_INVALID: Binary messages are not supported.\"]"
+                                .to_owned(),
+                        ))
+                        .await
+                        .expect("send failed");
+                }
+                Ok(Message::Ping(_)) | Ok(Message::Pong(_)) => debug!("Ping/Pong"),
+                Ok(Message::Close(_)) => {
+                    info!("Got request to close connection");
+                    return;
+                }
+                Err(tungstenite::error::Error::Capacity(
+                    tungstenite::error::CapacityError::MessageTooLong { size, max_size },
+                )) => {
+                    info!(
+                        "Message size too large, disconnecting this client. ({} > {})",
+                        size, max_size
+                    );
+                    self.stream.send(Message::Text("[\"NOTICE\", \"MAX_EVENT_SIZE_EXCEEDED: Exceeded maximum event size for this relay.  Closing Connection.\"]".to_owned())).await.expect("send notice failed");
+                    self.stream
+                        .close(Some(CloseFrame {
+                            code: CloseCode::Size,
+                            reason: "Exceeded max message size".into(),
+                        }))
+                        .await
+                        .expect("failed to send close frame");
+                    return;
+                }
+                Err(AlreadyClosed) => {
+                    warn!("this connection was already closed, and we tried to operate on it");
+                    return;
+                }
+                Err(ConnectionClosed) | Err(Io(_)) => {
+                    debug!("Closing this connection normally");
+                    return;
+                }
+                Err(Tls(_)) | Err(Protocol(_)) | Err(Utf8) | Err(Url(_)) | Err(HttpFormat(_))
+                | Err(Http(_)) => {
+                    info!("websocket/tls/enc protocol error, dropping connection");
+                    return;
+                }
+                Err(e) => {
+                    warn!("Some new kind of error, bailing: {:?}", e);
+                    return;
+                }
+            }
+        }
+    }
+
+    pub async fn send_notice(&mut self) {
+        self.stream.send(Message::Text(format!("foo"))).await;
     }
 
     // Error results will be transformed into client NOTICEs
