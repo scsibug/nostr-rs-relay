0c72053a49a6e7a0da4010e5921f45af20120e20
Greg Heartsfield <scsibug@imap.cc>
2022-12-17 23:17:16 -0600

    perf: increase mmap size to 1GB

--- src/schema.rs
+++ src/schema.rs
@@ -16,7 +16,7 @@ pub const STARTUP_SQL: &str = r##"
 PRAGMA main.synchronous=NORMAL;
 PRAGMA foreign_keys = ON;
 PRAGMA journal_size_limit=32768;
-pragma mmap_size = 536870912; -- 512MB of mmap
+pragma mmap_size = 1073741824; -- 1024MB of mmap
 "##;
 
 /// Latest database version
