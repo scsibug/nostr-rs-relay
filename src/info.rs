1623bacd0d892ca8765161bd7f3c4d1d81d5032a
Greg Heartsfield <scsibug@imap.cc>
2023-02-01 18:09:27 -0600

    improvement(NIP-33): advertise support for parameterized replaceable events

--- src/info.rs
+++ src/info.rs
@@ -35,7 +35,7 @@ impl From<config::Info> for RelayInfo {
             description: i.description,
             pubkey: i.pubkey,
             contact: i.contact,
-            supported_nips: Some(vec![1, 2, 9, 11, 12, 15, 16, 20, 22]),
+            supported_nips: Some(vec![1, 2, 9, 11, 12, 15, 16, 20, 22, 33]),
             software: Some("https://git.sr.ht/~gheartsfield/nostr-rs-relay".to_owned()),
             version: CARGO_PKG_VERSION.map(std::borrow::ToOwned::to_owned),
         }
