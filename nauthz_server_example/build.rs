23640a76ebd193fe2e0f93b667187bf926849ad2
Yuval Adam <_@yuv.al>
2023-02-14 22:18:00 +0200

    Add --experimental_allow_proto3_optional protoc arg in build configs, fixes #77

--- nauthz_server_example/build.rs
+++ nauthz_server_example/build.rs
@@ -1,4 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../proto/nauthz.proto")?;
+    tonic_build::configure()
+        .build_server(false)
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(
+            &["../proto/nauthz.proto"],
+            &["../proto"],
+        )?;
     Ok(())
 }
